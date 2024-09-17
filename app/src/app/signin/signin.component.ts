836a6e7c7eeb89c4b069956c96089a3bd4c17f6b
Wanchana Chanyaemsong <C.wanchana1998@gmail.com>
2024-09-17 21:01:17 +0700

    login

--- app/src/app/signin/signin.component.ts
+++ app/src/app/signin/signin.component.ts
@@ -1,12 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.css'
+  styleUrl: './signin.component.css',
 })
 export class SigninComponent {
+  token: string = '';
+  username: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) {}
+  signIn() {
+    if (this.username === '' || this.password === '') {
+      Swal.fire({
+        title: 'ตรวจสอบข้อมูล',
+        text: 'โปรดกรอก Username หรือ Password',
+        icon: 'warning',
+      });
+    } else {
+      // Proceed with sign-in logic here
+
+      const playload = {
+        username: this.username,
+        password: this.password,
+      };
+
+      try {
+        this.http
+          .post('https://localhost:3000/api/users/signin', playload)
+          .subscribe((res: any) => {
+            
+          }),
+          Swal.fire({
+            title: 'ตรวจสอบข้อมูล',
+            text: 'Invalid Username or Password!',
+            icon: 'error',
+          });
+      } catch (err: any) {
+        Swal.fire({
+          title: 'Success!',
+          text: 'You are logged in!',
+          icon: 'success',
+        });
+      }
+    }
+  }
 }
