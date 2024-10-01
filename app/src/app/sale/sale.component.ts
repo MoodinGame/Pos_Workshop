f54bfb3eef1e3a704b89c9f2fbfbbbf0da1f7e96
Wanchana Chanyaemsong <C.wanchana1998@gmail.com>
2024-10-01 20:26:37 +0700

    Sale

--- app/src/app/sale/sale.component.ts
+++ app/src/app/sale/sale.component.ts
@@ -1,12 +1,74 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import config from '../config';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sale',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './sale.component.html',
   styleUrl: './sale.component.css'
 })
 export class SaleComponent {
+
+  foods : any[] = [];
+  apiPath: string = ''
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+
+  ngOnInit() {
+    this.fetchData()
+    this.apiPath = config.apiServer;
+  }
+  fetchData() {
+    try {
+      this.http
+        .get(config.apiServer + '/api/food/list')
+        .subscribe((res: any) => {
+          this.foods = res.results;
+        })
+
+    } catch (e: any) {
+      Swal.fire({
+        title: 'Error',
+        text: e.message,
+        icon: 'error',
+      });
+    }
+  }
+
+  filterFoods(){
+    this.filter('food')
+  }
+
+  filterDrinks(){
+   this.filter('drink')
+  }
+
+  filter(foodTypes : string){
+    try {
+      this.http
+       .get(config.apiServer + '/api/food/filter/' + foodTypes)
+       .subscribe((res: any) => {
+          this.foods = res.results;
+        })
+    } catch (e : any) {
+      Swal.fire({
+        title: 'Error',
+        text: e.message,
+        icon: 'error',
+      }); 
+    }
+
+  }
+
+
 
 }
